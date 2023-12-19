14026f1895e4eec58301287496d7f8b331b919d4
Georgio Khoury <georgiok033@hotmail.com>
2023-12-19 20:03:09 +0200

    commit

--- backend/server.py
+++ backend/server.py
@@ -16,7 +16,7 @@ def connect_to_database():
         db = mysql.connector.connect(
             host="localhost",
             user="root",
-            passwd="",
+            passwd="d",
             database="App"
         )
         print("Connected to the database successfully.")
